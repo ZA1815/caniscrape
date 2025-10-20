d007114e370a520da5a20fa97e992482fbb25102
ZA1815 <zaahme18@gmail.com>
2025-10-20 15:45:16 -0400

    chore: update to 0.1.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='caniscrape',
-    version='0.1.6',
+    version='0.1.7',
     author='Zaid Ahmed',
     author_email='zaahme18@gmail.com',
     description='Analyze website anti-bot protections before you scrape',
